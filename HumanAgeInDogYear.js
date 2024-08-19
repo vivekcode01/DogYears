c0e2d915d42561a72e032c660d9a35613f859683
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-08-19 13:49:06 +0530

    Add files via upload

---
+++ HumanAgeInDogYear.js
@@ -0,0 +1,14 @@
+//constant that mean my age.
+const myAge = 254;
+//this created a variable which can be changed furthe
+let earlyYears =2;
+earlyYears *=10.5;
+//in this line we assingned vale to lateryear as myage -2, which we can change furthe
+let laterYears = myAge - 2;
+// this will multiply previous lateryear value with 4 and creat new lateryear
+laterYears *=4;
+console.log(earlyYears);
+console.log(laterYears);
+let myAgeInDogYears = laterYears+ earlyYears;
+const myName= "VIVEK".toLowerCase();
+console.log(`My name is ${myName}.I am ${myAge} years old in human years which is ${myAgeInDogYears} year old in dog years.`)
